ff9b89757aab2b28aecb5509af08dc8b54f09831
aimanaqimie@gmail.com <aimanaqimie@gmail.com>
2026-01-20 16:36:53 +0800

    Latest Calorie Tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'constants/app_colors.dart';
+import 'calorie-tracker/screens/home_screen.dart';
+import 'calorie-tracker/constants/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
